e874e8db6d189ea51b299f4305fc6010ac67355e
Navatusein <boghdan.kutsulima@gmail.com>
2024-02-10 14:50:34 +0200

    Create GitHub actions to automatically build docker images for dev

---
+++ Controllers/AuthenticationController.cs
@@ -0,0 +1,167 @@
+﻿using AuthenticationAPI.Database;
+using AuthenticationAPI.Models;
+using AuthenticationAPI.Service;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AuthenticationAPI.Controllers
+{
+    /// <summary>
+    /// Authentication Controller
+    /// </summary>
+    [Route("/api/authentication-api/v1")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private const string CookieKey = "RefreshToken";
+
+        private static Serilog.ILogger Logger => Serilog.Log.ForContext<AuthenticationController>();
+        private readonly AuthenticationApiDbContext _context;
+        private readonly JwtService _jwtService;
+
+        /// <summary>
+        /// Constructor of class AuthenticationController
+        /// </summary>
+        public AuthenticationController(AuthenticationApiDbContext context, JwtService jwtService)
+        {
+            _context = context;
+            _jwtService = jwtService;
+        }
+
+        /// <summary>
+        /// Login User
+        /// </summary>
+        /// <param name="loginDto"></param>
+        /// <response code="200">Login success</response>
+        /// <response code="400">Invalid login or password</response>
+        [HttpPost]
+        [Route("login")]
+        [AllowAnonymous]
+        [SwaggerResponse(statusCode: 200, type: typeof(UserDto), description: "Login success")]
+        [SwaggerResponse(statusCode: 400, type: typeof(ErrorDto), description: "Invalid login or password")]
+        public Task<IActionResult> PostLogin([FromBody] LoginDto loginDto)
+        {
+
+            //var cookieOptions = new CookieOptions
+            //{
+            //    HttpOnly = true,
+            //    Expires = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc).AddDays(30),
+            //    Secure = true,
+            //    SameSite = SameSiteMode.None
+            //};
+
+            //Response.Cookies.Append(CookieKey, refreshToken, cookieOptions);
+
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200, UserDto);
+            //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(400, ErrorDto);
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Refresh JWT Token
+        /// </summary>
+        /// <param name="refreshDto"></param>
+        /// <response code="200">OK</response>
+        /// <response code="400">Invalid refresh token</response>
+        [HttpPost]
+        [Route("refresh")]
+        [AllowAnonymous]
+        [SwaggerResponse(statusCode: 200, type: typeof(UserDto), description: "OK")]
+        [SwaggerResponse(statusCode: 400, type: typeof(ErrorDto), description: "Invalid refresh token")]
+        public Task<IActionResult> PostRefresh([FromBody] RefreshDto refreshDto)
+        {
+            var refreshToken = Request.Cookies[CookieKey];
+
+            //var cookieOptions = new CookieOptions
+            //{
+            //    HttpOnly = true,
+            //    Expires = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc).AddDays(30),
+            //    Secure = true,
+            //    SameSite = SameSiteMode.None
+            //};
+
+            //Response.Cookies.Append(CookieKey, refreshToken, cookieOptions);
+
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200, UserDto);
+            //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(400, ErrorDto);
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Register User
+        /// </summary>
+        /// <param name="registerDto"></param>
+        /// <response code="200">Register success</response>
+        /// <response code="400">Login already taken</response>
+        [HttpPost]
+        [Route("register")]
+        [AllowAnonymous]
+        [SwaggerResponse(statusCode: 200, type: typeof(UserDto), description: "Register success")]
+        [SwaggerResponse(statusCode: 400, type: typeof(ErrorDto), description: "Login already taken")]
+        public Task<IActionResult> PostRegister([FromBody] RegisterDto registerDto)
+        {
+
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200, UserDto);
+            //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(400, ErrorDto);
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Update User Data
+        /// </summary>
+        /// <param name="updateUserDto"></param>
+        /// <response code="200">OK</response>
+        /// <response code="400">Old and new passwords doesn&#39;t match</response>
+        /// <response code="401">Unauthorized</response>
+        [HttpPut]
+        [Route("update-user")]
+        [Authorize(Roles = "User, Admin")]
+        [SwaggerResponse(statusCode: 400, type: typeof(ErrorDto), description: "Old and new passwords doesn&#39;t match")]
+        [SwaggerResponse(statusCode: 401, type: typeof(ErrorDto), description: "Unauthorized")]
+        public virtual IActionResult PutUpdateUser([FromBody] UpdateUserDto updateUserDto)
+        {
+
+            //TODO: Uncomment the next line to return response 200 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(200);
+            //TODO: Uncomment the next line to return response 400 or use other options such as return this.NotFound(), return this.BadRequest(..), ...
+            // return StatusCode(400, ErrorDto);
+
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Generate password hash
+        /// </summary>
+        /// <param name="password"></param>
+        /// <response code="200">OK</response>
+        [HttpGet]
+        [Route("password")]
+        [AllowAnonymous]
+        [SwaggerResponse(statusCode: 200, type: typeof(Dictionary<string, string>), description: "OK")]
+        public ActionResult<Dictionary<string, string>> GetGeneratePassword(string password)
+        {
+            Logger.Debug("Start GetGeneratePassword(password: {@password})", password);
+            Dictionary<string, string> passwordData = new();
+
+            using var hmac = new HMACSHA512();
+            passwordData["Salt"] = Convert.ToBase64String(hmac.Key);
+            passwordData["Hash"] = Convert.ToBase64String(hmac.ComputeHash(Encoding.UTF8.GetBytes(password)));
+
+            Logger.Debug("Result GetGeneratePassword(passwordData: {@passwordData})", passwordData);
+            return StatusCode(200, passwordData);
+        }
+    }
+}
---
+++ Service/JwtService.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.Data;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace AuthenticationAPI.Service
+{
+    /// <summary>
+    /// Service for JWT token generation
+    /// </summary>
+    public class JwtService
+    {
+        private static Serilog.ILogger Logger => Serilog.Log.ForContext<JwtService>();
+        private readonly IConfiguration _config;
+
+        /// <summary>
+        /// Constructor of class JwtService
+        /// </summary>
+        public JwtService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        /// <summary>
+        /// Validate refresh JWT token
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="userId"></param>
+        /// <returns>Boolean valid token or not</returns>
+        public bool ValidateRefreshToken(string token, string userId)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var validationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = _config["RefreshJWT:Issuer"],
+                ValidAudience = _config["RefreshJWT:Audience"],
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["FrontendRefreshJWT:Key"]!))
+            };
+
+            if (!tokenHandler.CanReadToken(token))
+                return false;
+
+            try
+            {
+                var principal = tokenHandler.ValidateToken(token, validationParameters, out var validatedToken);
+
+                if (principal.HasClaim(c => c.Type == "id"))
+                    return userId == principal.Claims.First(c => c.Type == "userId").Value;
+            }
+            catch (Exception exception)
+            {
+                Logger.Error("Refresh token validation error: @1", exception);
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Generate authorization JWT token 
+        /// </summary>
+        /// <param name="userId">Id of user</param>
+        /// <param name="role">User role</param>
+        /// <returns>Authorization JWT token</returns>
+        public string GenerateAuthorizationToken(string userId, string role)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["AuthorizeJWT:Key"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim("userId", userId),
+                new Claim(ClaimTypes.Role, role)
+            };
+
+            var token = new JwtSecurityToken(
+                _config["AuthorizeJWT:Issuer"],
+                _config["AuthorizeJWT:Audience"],
+                claims,
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        /// <summary>
+        /// Generate refresh JWT token
+        /// </summary>
+        /// <param name="userId">Id of user</param>
+        /// <returns>Refresh JWT token</returns>
+        public string GenerateRefreshToken(string userId)
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["RefreshJWT:Key"]!));
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim("UserId", userId)
+            };
+
+            var token = new JwtSecurityToken(
+                _config["RefreshJWT:Issuer"],
+                _config["RefreshJWT:Audience"],
+                claims,
+                expires: DateTime.Now.AddDays(30),
+                signingCredentials: credentials);
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
